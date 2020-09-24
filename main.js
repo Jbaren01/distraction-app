27d4fdf298ea0f128488587c2e0b5c683972de8b
Jonathan Barenboim <dontpanicproductions@Jonathans-iMac.local>
2020-09-24 09:51:07 -0700

    jons stuff

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ button.style.fontSize = '35px';
 button.style.position = 'absolute';
 button.style.top = '0';
 button.style.zIndex = '1000';
-button.style.backgroundColor = 'red';
+button.style.backgroundColor = 'orange';
 button.style.opacity = '0.5';
 button.style.margin = '60px 0';
+button.style.color = 'white';
+
 
 // 2. an array to hold all the memes
 const memesArray = [
@@ -20,7 +22,10 @@ const memesArray = [
   'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMzIyOTU1Ni9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY2MDE5ODU0NH0.cRckokF9Xzih9FsJGQRapnItGysJ0TQlJmgAL-FojAc/img.jpg?width=980',
   'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMzIyOTU1Ny9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY1MzI1MzExOX0.LoNZ6vxa2KVQ0gEcmpouC3ycWmNebHqJiBSuKQxk50k/img.jpg?width=980',
   'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMzIyOTU2MC9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTYyNzE1NjkyNn0.jx1uMoqY07JG5NcByfovCsKhO8T4qKqW5lnNZk1VpM0/img.jpg?width=980',
-  'https://media.distractify.com/brand-img/oIR2HAmux/2160x1131/no-sports-memes2-1584547307501.jpg',
+  'https://media.distractify.com/brand-img/oIR2HAmux/2160x1131/no-sports-memes2-1584547307501.jpg', 'https://static.boredpanda.com/blog/wp-content/uploads/2020/06/3-5edf7041516fb__700.jpg', 'https://static.boredpanda.com/blog/wp-content/uploads/2020/06/jokes-about-2020-5edf2d2423557__700.jpg',
+  'https://static.boredpanda.com/blog/wp-content/uploads/2020/06/5edf2a994b1c9_0qolv2k4dx251-png__700.jpg', 'https://static.boredpanda.com/blog/wp-content/uploads/2020/06/21-5edf8215eddd9__700.jpg', 'https://i.pinimg.com/originals/0f/20/c1/0f20c1933df04a1b3d7ec48b027fdba7.jpg',
+  'https://ruinmyweek.com/wp-content/uploads/2020/06/face-mask-memes-you-can-laugh-at-under-your-face-mask-27-memes-6.jpg', 'https://content.api.news/v3/images/bin/4d401a14a40c0301f97214fd0bf11b78', 'https://www.sunway.com.my/stories/wp-content/uploads/2020/03/COVID-Meme.jpg',
+  'https://www.demilked.com/magazine/wp-content/uploads/2020/06/5ee08c44f2a05-1267505059333627904-png__700.jpg'
 ];
 
 //3. DOM Manipulation
@@ -32,8 +37,13 @@ body.appendChild(button);
 
 //make button on click run a function
 button.addEventListener('click', () => {
+  //on click play music
+  let audio = new Audio('time-to-go-01.mp3');
+  audio.play();
+
   let counter = 5;
   button.innerHTML = `${counter} seconds left`;
+  
 
   const interval = setInterval(() => {
     counter--;
@@ -50,6 +60,11 @@ button.addEventListener('click', () => {
       img.style.position = 'fixed';
       img.style.top = '10%';
       img.style.left = '10%';
+      img.style.width = '50%x';
+      img.style.height = 'auto';
+      img.style.border = 'thick solid #0000FF';
+      img.style.boxShadow = "10px 20px 30px grey";
+      // img.style.alignContent = 'center';
 
       //append image - gets put on the page
       body.appendChild(img);
@@ -57,6 +72,3 @@ button.addEventListener('click', () => {
   }, 1000);
 });
 
-//iterate through the array
-//randomly pick one element
-//display the image on the page
